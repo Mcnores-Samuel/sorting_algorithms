b9cb15427a7b08bf89e23c4524631f14bedf4e5c
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-07-25 00:35:59 +0200

    sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -33,7 +33,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *tmp, *tmp_prev;
 
-	if (*list == NULL || (*list != NULL && (*list)->next == NULL))
+	if (*list == NULL || (*list)->next == NULL)
 		return;
 
 	current = (*list)->next;
