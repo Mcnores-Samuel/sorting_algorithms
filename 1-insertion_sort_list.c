7c7bfe1f9a2c6782f9a0154fe55db2177b82d3b6
Mcnores-Samuel <samuelmcnores1@gmail.com>
2023-07-25 00:32:41 +0200

    sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,100 +1,51 @@
 #include "sort.h"
 
+/**
+ * swap_list - swaps specified node in a linked list.
+ * @list: double to the list at which nodes are to be swapped.
+ * @first_node: double pointer to the first_node
+ * @second_node: pointer to the second node to swap.
+ * Return: void.
+ */
+void swap_list(listint_t **list, listint_t **first_node, listint_t *second_node)
+{
+	(*first_node)->next = second_node->next;
+	if (second_node->next != NULL)
+		second_node->next->prev = *first_node;
+	second_node->prev = (*first_node)->prev;
+	second_node->next = *first_node;
+	if ((*first_node)->prev != NULL)
+		(*first_node)->prev->next = second_node;
+	else
+		*list = second_node;
+	(*first_node)->prev = second_node;
+	*first_node = second_node->prev;
+}
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm;
+ * @list: double pointer to the list to be sorted.
+ * Return: nothing.
+ */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *tmp, *a, *c, *b, *d;
+	listint_t *current, *tmp, *tmp_prev;
 
 	if (*list == NULL || (*list != NULL && (*list)->next == NULL))
 		return;
 
-	current = *list;
+	current = (*list)->next;
 	while (current != NULL)
 	{
-		tmp = current->prev;
-		while (tmp != NULL && tmp->n > tmp->next->n)
+		tmp = current->next;
+		tmp_prev = current->prev;
+		while (tmp_prev != NULL && current->n < tmp_prev->n)
 		{
-			a = tmp->prev;
-			b = tmp;
-			c = tmp->next;
-			d = tmp->next->next;
-
-			if (a != NULL && b)
-			{
-				a->next = c;
-				c->prev = a;
-				c->next = b;
-				b->prev = c;
-			}
-			else
-			{
-				a = current;
-				current->next = tmp;
-				*list = a;
-			}
-			if (d != NULL)
-			{
-				b->next = d;
-				d->prev = b;
-			}
-			else
-				b->next = NULL;
-			tmp = c;
-			tmp = tmp->prev;
+			swap_list(list, &tmp_prev, current);
 			print_list(*list);
+
 		}
-		current = current->next;
+		current = tmp;
 	}
-}
-
-/**
- * create_listint - Creates a doubly linked list from an array of integers
- *
- * @array: Array to convert to a doubly linked list
- * @size: Size of the array
- *
- * Return: Pointer to the first element of the created list. NULL on failure
- */
-listint_t *create_listint(const int *array, size_t size)
-{
-    listint_t *list;
-    listint_t *node;
-    int *tmp;
-
-    list = NULL;
-    while (size--)
-    {
-        node = malloc(sizeof(*node));
-        if (!node)
-            return (NULL);
-        tmp = (int *)&node->n;
-        *tmp = array[size];
-        node->next = list;
-        node->prev = NULL;
-        list = node;
-        if (list->next)
-            list->next->prev = list;
-    }
-    return (list);
-}
-
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-    listint_t *list;
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
-
-    list = create_listint(array, n);
-    if (!list)
-        return (1);
-    print_list(list);
-    printf("\n");
-    insertion_sort_list(&list);
-    printf("\n");
-    print_list(list);
-    return (0);
 }
